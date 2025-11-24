4acb654ab275f1af15154bc0db7d21350264789b
VN-78 <vishwanath.s.god@gmail.com>
2025-11-25 01:27:59 +0530

    Installed the Shadecn Component Lib

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+import Button from "@/components/ui/button/button.svelte";
+
+const name = "Vishwa"
+let number = $state(0)
+
+</script>
+
+<div class="flex flex-col items-center justify-center  ">
+
+<h1 class="text-7xl  p-4 m-4 " >
+    Title of portfolio
+</h1>
+
+<h2>
+    {name}
+</h2>
+<!-- so this is the syntax for comments this is very complex -->
+<h4 class="text-xl items-center justify-center " >
+    An Simple Counter Click on the button to increment the number 
+</h4>
+
+<Button 
+onclick={()=>{number++}} 
+class=" flex items-center justify-center cursor-pointer w-24 h-12 p-4 m-4 "
+>
+{number}
+</Button>
+
+</div>

5853ca87e25dbd247279754d611adec1227ef213
VN-78 <vishwanath.s.god@gmail.com>
2025-11-28 02:59:29 +0530

    Created the file structure and theme switcher and navbar

---
+++ Frontend/src/lib/components/atoms/buttons/ThemeButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Button from "@/components/ui/button/button.svelte";
+    import { toggleMode } from "mode-watcher";
+
+    import halfCircle from "@/assets/half-circle-svgrepo-com.svg";
+    let {
+        class: className  = "",
+        variant = "default",
+    } = $props<{
+        class? : String;
+        variant? : 'default' | 'outline' | 'ghost' | 'link';
+    }>();
+
+</script>
+
+<div>
+    <Button onclick={toggleMode} class={className}  variant={variant} size="icon" >
+        <img src={halfCircle} alt="toggle mode" class=" h-6 w-6 dark:rotate-180" />
+    </Button>
+</div>
---
+++ Frontend/src/lib/components/molecules/navbar/NavBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    // import * as NavigationMenu from "@/components/ui/navigation-menu";
+    import ThemeButton from "@/components/atoms/buttons/ThemeButton.svelte";
+    import * as Avatar from "@/components/ui/avatar/index";
+    import vnIcon from "@/assets/vn-svgrepo-com.svg";
+</script>
+
+<div
+    class=" bg-foreground text-primary w-full h-fit flex justify-between items-center p-2  px-4 "
+>
+    <Avatar.Root class=" bg-foreground-primary w-12 h-12  p-2" >
+        <Avatar.Image src={vnIcon} alt="@vn-78" />
+        <Avatar.Fallback>VN</Avatar.Fallback>
+    </Avatar.Root>
+    <ThemeButton variant="outline"></ThemeButton>
+
+</div>

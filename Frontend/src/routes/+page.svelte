2813fd72bc62e12440a416f63906a16665038f8f
VN-78 <vishwanath.s.god@gmail.com>
2025-11-28 02:58:48 +0530

    Added an Sample Navbar

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,31 +1,9 @@
-<script lang="ts">
-
-import Button from "@/components/ui/button/button.svelte";
-
-const name = "Vishwa"
-let number = $state(0)
-
+<script>
+    import NavBar from "@/components/molecules/navbar/NavBar.svelte";
 </script>
 
-<div class="flex flex-col items-center justify-center  ">
+<div class=" bg-background ">
+    <NavBar>
 
-<h1 class="text-7xl  p-4 m-4 " >
-    Title of portfolio
-</h1>
-
-<h2>
-    {name}
-</h2>
-<!-- so this is the syntax for comments this is very complex -->
-<h4 class="text-xl items-center justify-center " >
-    An Simple Counter Click on the button to increment the number 
-</h4>
-
-<Button 
-onclick={()=>{number++}} 
-class=" flex items-center justify-center cursor-pointer w-24 h-12 p-4 m-4 "
->
-{number}
-</Button>
-
+    </NavBar>
 </div>

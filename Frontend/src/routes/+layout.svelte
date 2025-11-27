74b356b503e8480091f5e5ef58f2224ab3320fae
VN-78 <vishwanath.s.god@gmail.com>
2025-11-28 02:58:16 +0530

    Added Theme Switching

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
+	import vnicon from "$lib/assets/vn-svgrepo-com.svg"
+	import { ModeWatcher } from 'mode-watcher';
 	
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={vnicon} />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
+<ModeWatcher />
 {@render children()}

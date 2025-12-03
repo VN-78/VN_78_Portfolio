f7e1ca0cf6288fd756d0458122f8ab285a463a55
VN-78 <vishwanath.s.god@gmail.com>
2025-12-04 02:48:44 +0530

    Other Changes

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
     import NavBar from "@/components/molecules/navbar/NavBar.svelte";
+    import GoldenRatioAnimation from "@/components/animations/HeroAnimation/GoldenRatioAnimation.svelte";
 </script>
 
 <div class=" bg-background ">
-    <NavBar>
-
-    </NavBar>
+    <NavBar/>
+    <GoldenRatioAnimation/>
 </div>

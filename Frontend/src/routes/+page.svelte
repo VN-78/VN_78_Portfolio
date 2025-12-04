b43d7d706c15aac02246e73a1631c4a6d0db95be
VN-78 <vishwanath.s.god@gmail.com>
2025-12-05 01:15:23 +0530

    Synced With the math

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,9 +1,25 @@
 <script>
     import NavBar from "@/components/molecules/navbar/NavBar.svelte";
     import GoldenRatioAnimation from "@/components/animations/HeroAnimation/GoldenRatioAnimation.svelte";
+    import { fly } from "svelte/transition";
+    import { cubicOut } from "svelte/easing";
+    let showText = $state(false);
 </script>
 
-<div class=" bg-background ">
-    <NavBar/>
-    <GoldenRatioAnimation/>
+<div class=" bg-background">
+    <NavBar />
+    <GoldenRatioAnimation animationFinished={() => (showText = true)} />
+
+    {#if showText}
+        <div
+            class="z-10  "
+            in:fly={{ y: 100, duration: 1000, easing: cubicOut }}
+        >
+            <h1
+                class="text-4xl md:text-6xl font-bold  text-primary-foreground mb-4"
+            >
+                {` Software Developer `}
+            </h1>
+        </div>
+        {/if}
 </div>

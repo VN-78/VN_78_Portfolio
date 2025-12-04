7b573136a41a683c1e55f85de2f552711f6ba4a4
VN-78 <vishwanath.s.god@gmail.com>
2025-12-05 01:14:54 +0530

    Synced With the math

--- Frontend/src/lib/components/animations/HeroAnimation/GoldenRatioAnimation.svelte
+++ Frontend/src/lib/components/animations/HeroAnimation/GoldenRatioAnimation.svelte
@@ -7,15 +7,19 @@
     let width = $state(0);
     let height = $state(0);
     let ready = $state(false);
-    let animationFinished = $state(false);
+    let { animationFinished }: { animationFinished?: () => void } = $props();
     let num = 18;
     // console.log(fibonacci(num).filter((n) => n > 0));
     const rawData = fibonacci(num).filter((n) => n > 0);
     const pathData = generateSpiralPath(rawData);
 
-    const maxRadius = rawData[rawData.length - 1];
-    // dynamic calculation of scale to occupy 50% of the screen
-    const scale = $derived((Math.min(width, height) / maxRadius) * 0.8 || 0.1);
+    // 2. Responsive Scaling
+    // Calculate how much to zoom to fit 80% of the screen
+    // We check both width and height to ensure it fits on mobile too
+    const scale = $derived(
+        (Math.min(width, height) / Math.max(pathData.width, pathData.height)) *
+            1.3 || 0.1,
+    );
 
     onMount(() => {
         ready = true;
@@ -27,14 +31,18 @@
     bind:clientHeight={height}
     bind:clientWidth={width}
 >
-    <svg {height} {width} class="w-full h-full">
+    <svg
+        {height}
+        {width}
+        class="w-full h-full absolute inset-0 pointer-events-none transition-transform duration-300"
+    >
         <g
-            transform=" translate({width / 1.5}, {height /
-                1.5}) scale({scale}) scale(-1,1) rotate(90)"
+            transform=" translate({width / 2}, {height /
+                2}) scale({scale}) scale(-1,1) rotate(90) translate({-pathData.centerX}, {-pathData.centerY})"
         >
             {#if ready}
                 <path
-                    d={pathData}
+                    d={pathData.path}
                     stroke="oklch(64.71% 0.21732 36.839)"
                     stroke-width={2 / scale}
                     fill="none"
@@ -45,7 +53,7 @@
                     }}
                     onintroend={() => {
                         console.log("Spiral Done. Triggering Text.");
-                        animationFinished = true;
+                        animationFinished?.();
                     }}
                 />
             {/if}
